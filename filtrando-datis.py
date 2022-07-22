ebd310d0062f7e2e4271c29ec87d1d86b7005bb9
Juan Mejia <juanmejiadev@gmail.com>
2022-07-22 10:46:30 -0500

    Se agregan las clases de High Order Functions y una actividad filtrando datos con ellas

---
+++ filtrando-datis.py
@@ -0,0 +1,91 @@
+from argparse import ArgumentDefaultsHelpFormatter
+from sqlite3 import DatabaseError
+
+
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'HÃ©ctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run():
+    #pyton = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    pyton = filter(lambda worker: worker["language"] == "python", DATA)
+    pyton = list(map(lambda worker: worker["name"], pyton))
+    print(pyton)
+
+    adults = [worker for worker in DATA if worker["age"]>18]
+    old_people = [worker | {'old': worker['age'] > 70} for worker in DATA if worker["age"]>18]
+    print (adults)
+    print("################################")
+    print(old_people)
+
+if __name__ == '__main__':
+    run()
---
+++ lambdaclass.py
@@ -0,0 +1,4 @@
+#Estructura Identificador = argumento : expresion
+#Ejemplo
+palindrome = lambda string: string == string[::-1]
+print(palindrome('aja'))
